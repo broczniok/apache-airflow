a3e499cd923ff192e3066bf52f64a75751baeb84
broczniok <broczniok@griddynamics.com>
2024-07-18 13:34:38 +0200

    adding not working task_group_dag

---
+++ task_group_dag.py
@@ -0,0 +1,76 @@
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.subdag import SubDagOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+import logging
+
+# Initialize logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+def create_section():
+    """
+    Create tasks in the outer section.
+    """
+    dummies = [EmptyOperator(task_id=f'task-{i + 1}') for i in range(5)]
+    logger.info("Created outer section tasks: %s", dummies)
+
+    with TaskGroup("inside_section_1") as inside_section_1:
+        inside_1_tasks = [EmptyOperator(task_id=f'task-{i + 1}') for i in range(3)]
+        logger.info("Created inside_section_1 tasks: %s", inside_1_tasks)
+
+    with TaskGroup("inside_section_2") as inside_section_2:
+        inside_2_tasks = [EmptyOperator(task_id=f'task-{i + 1}') for i in range(3)]
+        logger.info("Created inside_section_2 tasks: %s", inside_2_tasks)
+
+    dummies[-1] >> inside_section_1
+    dummies[-2] >> inside_section_2
+
+def tasks_sub_dag(parent_dag_name, child_dag_name, start_date, schedule_interval):
+    subdag = DAG(
+        dag_id=f'{parent_dag_name}.{child_dag_name}',
+        schedule_interval=schedule_interval,
+        start_date=start_date,
+        catchup=False,
+    )
+    with subdag:
+        subtask_1 = EmptyOperator(task_id='subtask_1', queue='jobs_dag', dag=subdag)
+        subtask_2 = EmptyOperator(task_id='subtask_2', queue='jobs_dag', dag=subdag)
+        subtask_3 = EmptyOperator(task_id='subtask_3', queue='jobs_dag', dag=subdag)
+
+        logger.info("Created subdag tasks: %s, %s, %s", subtask_1, subtask_2, subtask_3)
+
+        subtask_1 >> subtask_2 >> subtask_3
+
+    return subdag
+
+with DAG(dag_id="task_group_dag", start_date=days_ago(2), schedule_interval='@daily', default_args=default_args,catchup=False) as dag:
+    start = EmptyOperator(task_id="start")
+
+    with TaskGroup("section_1", tooltip="Tasks for Section 1") as section_1:
+        create_section()
+
+    sub_dag_task = SubDagOperator(
+        task_id='sub_dag',
+        subdag=tasks_sub_dag(
+            parent_dag_name='task_group_dag',
+            child_dag_name='sub_dag',
+            start_date=dag.start_date,
+            schedule_interval=dag.schedule_interval
+        ),
+        queue='jobs_dag'
+    )
+    some_other_task = EmptyOperator(task_id="some-other-task")
+
+    end = EmptyOperator(task_id='end')
+
+    start >> section_1 >> some_other_task >> sub_dag_task >> end
+    logger.info("Main DAG tasks created: start, section_1, some_other_task, sub_dag_task, end")
+
